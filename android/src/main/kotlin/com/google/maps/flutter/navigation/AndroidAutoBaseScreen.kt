f7c53fe00dd21b7dd4f7f5b4a18427317234e923
bderrien <bderrien@betomorrow.com>
2026-06-04 10:03:01 +0200

    Fix "android-events-from-flutter-to-native"

--- android/src/main/kotlin/com/google/maps/flutter/navigation/AndroidAutoBaseScreen.kt
+++ android/src/main/kotlin/com/google/maps/flutter/navigation/AndroidAutoBaseScreen.kt
@@ -47,6 +47,26 @@ open class AndroidAutoBaseScreen(carContext: CarContext) :
      * created to customize map appearance.
      */
     var mapOptions: AutoMapViewOptions? = null
+
+
+    /**
+     * Event handler for sending custom events from Flutter to native.
+     */
+    private var flutterEventForwarder: ((String, Any) -> Unit)? = null
+
+    fun registerFlutterEventHandler(forwarder: ((String, Any) -> Unit)?) {
+      flutterEventForwarder = forwarder
+    }
+
+    private fun clearFlutterEventForwarder(forwarder: (String, Any) -> Unit) {
+      if (flutterEventForwarder === forwarder) {
+        flutterEventForwarder = null
+      }
+    }
+
+    fun dispatchEventFromFlutter(event: String, data: Any) {
+      flutterEventForwarder?.invoke(event, data)
+    }
   }
 
   /**
@@ -90,9 +110,15 @@ open class AndroidAutoBaseScreen(carContext: CarContext) :
   private var mIsPromptVisible: Boolean = false
   private var mIsSurfaceDestroyed: Boolean = false
 
+  private val flutterEventForwarderForThisScreen: (String, Any) -> Unit = { event, data ->
+    onCustomNavigationAutoEventFromFlutter(event, data)
+  }
+
   init {
     initializeSurfaceCallback()
     initializeNavigationListener()
+
+    registerFlutterEventHandler(flutterEventForwarderForThisScreen)
   }
 
   private fun initializeNavigationListener() {
@@ -110,6 +136,7 @@ open class AndroidAutoBaseScreen(carContext: CarContext) :
     object : DefaultLifecycleObserver {
       override fun onDestroy(owner: LifecycleOwner) {
         GoogleMapsNavigationSessionManager.navigationReadyListener = null
+        clearFlutterEventForwarder(flutterEventForwarderForThisScreen)
         mIsNavigationReady = false
       }
     }
--- android/src/main/kotlin/com/google/maps/flutter/navigation/GoogleMapsAutoViewMessageHandler.kt
+++ android/src/main/kotlin/com/google/maps/flutter/navigation/GoogleMapsAutoViewMessageHandler.kt
@@ -489,12 +489,6 @@ class GoogleMapsAutoViewMessageHandler(private val viewRegistry: GoogleMapsViewR
   }
 
   override fun sendCustomNavigationAutoEvent(event: String, data: Any) {
-    // This method receives custom events from Flutter.
-    // The implementation is left empty by design, as developers should handle
-    // custom events in their AndroidAutoBaseScreen subclass by overriding
-    // onCustomNavigationAutoEventFromFlutter method.
-    //
-    // Note: If you need to handle events here, you would need to maintain a reference
-    // to your AndroidAutoBaseScreen instance and call a method on it.
+    AndroidAutoBaseScreen.dispatchEventFromFlutter(event, data)
   }
 }
